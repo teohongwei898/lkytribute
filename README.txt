a3c7e193f5b47786cf32d8d0cbe374d40027ea23
Hong Wei <teohongwei898@gmail.com>
2017-09-27 16:06:28 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/teohongwei898/pen/gxWMgy.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $("li").addClass("grow");
+  $("li").addClass("grow:hover");
+  $("blockquote").addClass("grow");
+  $("blockquote").addClass("grow:hover");
+  $(".links").addClass("grow");
+  $(".links").addClass("grow:hover");
+
+});
